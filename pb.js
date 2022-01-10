696b07bdff544d62c2b40be413dccbd2246a5100
razaizim1 <zim.sarkar5956@gmail.com>
2022-01-10 09:48:22 +0600

    problem solving 04

---
+++ pb.js
@@ -0,0 +1,12 @@
+var numOne = 20;
+var numTwo = 40;
+
+var addition = numOne + numTwo;
+var subtraction = numOne - numTwo;
+var multiplication = numOne * numTwo;
+var division = numOne/numTwo;
+
+console.log(addition)
+console.log(subtraction)
+console.log(multiplication)
+console.log(division)
